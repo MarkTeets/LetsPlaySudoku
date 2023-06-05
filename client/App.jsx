e3804b88555a68a3f90863537d5277f24e4b7159
Mark Teets <markteets@gmail.com>
2023-06-05 04:55:24 -0400

    Removed unused code

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 
 //pages, loaders
 import Home from './pages/Home';
-import { PuzzlePageContainer, puzzleLoader, puzzleTestLoader } from './pages/PuzzlePageContainer';
-// import PuzzlePageContainer from './components/gameplay/PuzzlePageContainer';
+import { PuzzlePageContainer, puzzleLoader, puzzleTestLoader } from './pages/puzzle/PuzzlePageContainer';
 
 //layouts
 import RootLayout from './layouts/RootLayout';
@@ -25,31 +24,13 @@ const router = createBrowserRouter(
       />
     </Route>
   )
-)
-
+);
 
 
 const App = () => {
   return (
-      <RouterProvider router={router}/>
-  )
-}
+    <RouterProvider router={router}/>
+  );
+};
 
 export default App;
-
-
-
-
-/*
-import PuzzlePageContainer from './components/gameplay/PuzzlePageContainer';
-
-const App = () => {
-  return (
-      <div id='app'>
-        <PuzzlePageContainer key='PuzzlePageContainer'/>
-      </div>
-  )
-}
-
-export default App;
-*/
--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import house from '../data/house.png'
+import React from 'react';
+import house from '../data/house.png';
 
 function Home () {
   return (
     <div className='home-page'>
       <h1>This is my home!!!</h1>
-        <img id='house-img' src={house} />
+      <img id='house-img' src={house} />
       <h1>Welcome everyone!</h1>
       <h2>Login functionality coming soon!</h2>
     </div>
-    );
+  );
 }
 
 export default Home;
