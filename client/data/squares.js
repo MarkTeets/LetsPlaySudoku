71a0681af8d36d6a8ba93159428bb1d720b88770
Mark Teets <markteets@gmail.com>
2023-06-06 07:19:33 -0400

    Reorganized in order of use

--- client/data/squares.js
+++ client/data/squares.js
@@ -1,135 +1,7 @@
 /**
-* This file exports a function that when invoked, returns a new instance of an newSquares object
+* This file exports a function that when invoked, returns a new instance of an allSquares object
 * should take a string parameter of length 81 representing sudoku puzzle, each char is 0-9, with 0 signifying empty space
 */
-
-//These variables are declared here to be used in the class definition to avoid recreating commonly used information
-const { madeSquares, keys, boxes } = makeSquares();
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const numStringRegex = /[0123456789]/;
-export const unitBoxes = boxes;
-
-/** class allSquares
- * Produces an object holding each square of a sudoku grid. These squares are each delivered an
- * appropriate display value from a sudoku puzzle string, which is an 81 character string holding
- * numbers 0-9, 0 indicating an empty square.
- */
-
-class allSquares {
-  constructor(puzzleString = emptyPuzzleMaker()) {
-
-    if (!isValidPuzzle(puzzleString)) {
-      console.log('Puzzle string was not valid');
-      throw new Error('Puzzle string was not valid');
-    }
-
-    let squareId, puzzleVal;
-
-    for (let i = 0; i < keys.length; i += 1) {
-      squareId = keys[i];
-      puzzleVal = puzzleString[i];
-      const possibleVal = new Set(numbers);
-
-      if (puzzleVal === '0') {
-        this[squareId] = {
-          ...madeSquares[squareId],
-          possibleVal
-        };
-      } else {
-        this[squareId] = {
-          ...madeSquares[squareId],
-          displayVal: puzzleVal,
-          fixedVal: true,
-          // Unless the way I check for a completed game uses these, I shouldn't add possibleVal to fixed Val numbers
-          possibleVal
-        };
-      }
-    }
-  }
-
-  /** Prototype methods:
-  * I tried to add methods here and then realized that I wanted these methods to alter allState instances
-  * which can't be done in react. New objects should be used every time state is updates
-  */ 
-}
-
-/** createNewSquares
- * 
- * @param {string} puzzleString sudoku puzzle string, which is an 81 character string holding
- * characters 0-9
- * @returns a new instance of the allSquares class.
- */
-
-export const createNewSquares = (puzzleString) => {
-  return new allSquares(puzzleString);
-};
-
-/**
- * Given an allSquares object, a squareID, and a newly entered value in an input, 
- * 
- * @param {object} allSquares 
- * @param {string} squareId 
- * @param {string} newVal 
- * @returns 
- */
-export function newAllSquares(allSquares, squareId, newVal) {
-
-  // If the state value hasn't changed, skip the function and just return the original object
-  if (allSquares[squareId].displayVal === newVal) {
-    // alert('state has not changed');
-    return allSquares;
-  }
-  // Make a deep copy of the allSquares object
-  const newAllSquareObj = {};
-  for (const id of keys) {
-    newAllSquareObj[id] = {
-      ...allSquares[id],
-      possibleVal: new Set(allSquares[id].possibleVal),
-      peers: new Set(allSquares[id].peers)
-    };
-  }
-  // Change the specific values for the square that was changed
-  newAllSquareObj[squareId].displayVal = newVal;
-  newAllSquareObj[squareId].duplicate = false;
-  // Iterate over the entire grid and check each square to see if it had duplicate values within the squares in its peers Set
-  // If so, change the duplicate value of said square to true
-  findDuplicates(newAllSquareObj);
-  return newAllSquareObj;
-}
-
-export const isPuzzleFinished = (allSquares) => {
-  for (const squareId of keys) {
-    if (allSquares[squareId].displayVal === '0' || allSquares[squareId].duplicate) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*Testing
-
-const emptyPuzzle = emptyPuzzleMaker();
-// console.log(emptyPuzzle.length)
-const samplePuzzle = '077000044400009610800634900094052000358460020000800530080070091902100005007040822';
-
-// console.log(keys.length)
-
-const grid = createNewSquares(samplePuzzle);
-findDuplicates(grid);
-// console.log(grid);
-
-const newGrid = newAllSquares(grid, 'A1', '1')
-console.log(newGrid['A1'].displayVal)
-
-// */
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Helper function definitions
 
 /** makeSquares
  * 
@@ -143,7 +15,7 @@ console.log(newGrid['A1'].displayVal)
  *  3. keys array holding all of the Id strings of each square
  */
 
-function makeSquares () {
+const makeSquares = () => {
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
   const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const rows = [];
@@ -216,9 +88,104 @@ function makeSquares () {
   return {
     madeSquares,
     boxes,
-    keys
+    keys,
+    numbers
   };
+};
+
+
+//These variables are declared here to be used in the class definition to avoid recreating commonly used information
+const { madeSquares, keys, boxes, numbers } = makeSquares();
+const numStringRegex = /[0123456789]/;
+export const unitBoxes = boxes;
+
+
+/** emptyPuzzleMaker
+ * @returns an empty puzzle string, comprises 81 0's
+ */
+
+const emptyPuzzleMaker = () => {
+  return '0'.repeat(81);
+};
+
+
+/** isValidPuzzle
+ * 
+ * Checks input parameter to see if string if exactly 81 characters long and each character is
+ * a string representaion of the numbers 0-9
+ * 
+ * @param {string} puzzleString A string to be tested to see if it's a valid sudoku puzzle
+ * @returns boolean
+ */
+
+const isValidPuzzle = (puzzleString) => {
+  if (puzzleString.length !== 81) {
+    console.log('puzzle string length isn\'t 81');
+    return false;
+  }
+
+  let result = true;
+
+  for (let i = 0; i < puzzleString.length; i += 1){
+    if (!(numStringRegex.test(puzzleString[i]))) {
+      console.log('character', `'${puzzleString[i]}'`, 'was found in puzzle string. Only strings representing nums 0-9 are allowed.');
+      result = false;
+    }
+  }
+  return result;
+};
+
+
+/** class allSquares
+ * Produces an object holding each square of a sudoku grid. These squares are each delivered an
+ * appropriate display value from a sudoku puzzle string, which is an 81 character string holding
+ * numbers 0-9, 0 indicating an empty square.
+ */
+
+class allSquares {
+  constructor(puzzleString = emptyPuzzleMaker()) {
+
+    if (!isValidPuzzle(puzzleString)) {
+      console.log('Puzzle string was not valid');
+      throw new Error('Puzzle string was not valid');
+    }
+
+    let squareId, puzzleVal;
+
+    for (let i = 0; i < keys.length; i += 1) {
+      squareId = keys[i];
+      puzzleVal = puzzleString[i];
+      const possibleVal = new Set(numbers);
+
+      if (puzzleVal === '0') {
+        this[squareId] = {
+          ...madeSquares[squareId],
+          possibleVal
+        };
+      } else {
+        this[squareId] = {
+          ...madeSquares[squareId],
+          displayVal: puzzleVal,
+          fixedVal: true,
+          possibleVal
+        };
+      }
+    }
+  }
 }
+
+
+/** createNewSquares
+ * 
+ * @param {string} puzzleString sudoku puzzle string, which is an 81 character string holding
+ * characters 0-9
+ * @returns a new instance of the allSquares class.
+ */
+
+export const createNewSquares = (puzzleString) => {
+  return new allSquares(puzzleString);
+};
+
 
 /** Notes on find duplicates:
  * I can actually do this recursively and only visit the peers of my changed square if I found a change that needed to
@@ -237,7 +204,7 @@ function makeSquares () {
  * On second thought, recursion might be slower without tail-call optimization. I should look into this
 */
 
-function findDuplicates(allSquares) {
+const findDuplicates = (allSquares) => {
   // let squareId
   for (const squareId of keys) {
     const square = allSquares[squareId];
@@ -256,62 +223,75 @@ function findDuplicates(allSquares) {
       }
     });
   }
-}
+};
 
 
-/** isValidPuzzle
+/** newAllSquares
+ * Given an allSquares object, a squareID, and a newly entered value in an input, return a deep copy with appropriate values
  * 
- * Checks input parameter to see if string if exactly 81 characters long and each character is
- * a string representaion of the numbers 0-9
- * 
- * @param {string} puzzleString A string to be tested to see if it's a valid sudoku puzzle
- * @returns boolean
+ * @param {object} allSquares Current state of the puzzle 
+ * @param {string} squareId Id of the changed square 
+ * @param {string} newVal New value of the changed square 
+ * @returns {object} New state of puzzle
  */
 
-function isValidPuzzle(puzzleString) {
-  if (puzzleString.length !== 81) {
-    console.log('puzzle string length isn\'t 81');
-    return false;
-  }
-
-  let result = true;
-
-  for (let i = 0; i < puzzleString.length; i += 1){
-    if (!(numStringRegex.test(puzzleString[i]))) {
-      console.log('character', `'${puzzleString[i]}'`, 'was found in puzzle string. Only strings representing nums 0-9 are allowed.');
-      result = false;
-    }
-  }
-  return result;
-}
-
-/** emptyPuzzleMaker
- * @returns an empty puzzle string, comprises 81 0's
- */
-
-function emptyPuzzleMaker() {
-  return '0'.repeat(81);
-}
-
-
-/* I used to have this function check to make sure the input was ok, but that's unnecessary now that I have my value display container
-do it before passing calling the onInputChange function
-
-export function newAllSquares(allSquares, squareId, newVal) {
-
-  if (allSquares[squareId].displayVal === newVal ||
-    !(numStringRegex.test(newVal)) ||
-    typeof newVal !== 'string' || //these last two tests are for me, they'll never actually come up when using the application
-    newVal.length !== 1) {
-    alert('state has not changed')
+export const newAllSquares = (allSquares, squareId, newVal) => {
+  // If the state value hasn't changed, skip the function and just return the original object
+  if (allSquares[squareId].displayVal === newVal) {
+    // alert('state has not changed');
     return allSquares;
   }
-
-  const newSquare = { ...allSquares[squareId] };
-  newSquare.displayVal = newVal;
-  newSquare.duplicate = false;
-  const newAllSquareObj = { ...allSquares, [squareId]: newSquare };
-  findDuplicates(newAllSquareObj)
+  // Make a deep copy of the allSquares object
+  const newAllSquareObj = {};
+  for (const id of keys) {
+    newAllSquareObj[id] = {
+      ...allSquares[id],
+      possibleVal: new Set(allSquares[id].possibleVal),
+      peers: new Set(allSquares[id].peers)
+    };
+  }
+  // Change the specific values for the square that was changed
+  newAllSquareObj[squareId].displayVal = newVal;
+  newAllSquareObj[squareId].duplicate = false;
+  // Iterate over the entire grid and check each square to see if it had duplicate values within the squares in its peers Set
+  // If so, change the duplicate value of said square to true
+  findDuplicates(newAllSquareObj);
   return newAllSquareObj;
-}
-*/
+};
+
+
+/** isPuzzleFinished
+ *  Checks if a puzzle is complete by seeing if there are no empty spaces and no duplicates in the puzzle
+ * 
+ * @param {object} allSquares 
+ * @returns {boolean}
+ */
+
+export const isPuzzleFinished = (allSquares) => {
+  for (const squareId of keys) {
+    if (allSquares[squareId].displayVal === '0' || allSquares[squareId].duplicate) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*Testing
+
+const emptyPuzzle = emptyPuzzleMaker();
+// console.log(emptyPuzzle.length)
+const samplePuzzle = '077000044400009610800634900094052000358460020000800530080070091902100005007040822';
+
+// console.log(keys.length)
+
+const grid = createNewSquares(samplePuzzle);
+findDuplicates(grid);
+// console.log(grid);
+
+const newGrid = newAllSquares(grid, 'A1', '1')
+console.log(newGrid['A1'].displayVal)
+
+// */
