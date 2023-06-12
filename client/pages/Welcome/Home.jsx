049425b1eaed08595e08af1c954b19b7efe21d26
Mark Teets <markteets@gmail.com>
2023-06-12 00:59:40 -0400

    Updated to reflect login feature

--- client/pages/Welcome/Home.jsx
+++ client/pages/Welcome/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
       <h1>This is my home!!!</h1>
       <img id='house-img' src={house} alt='A house' />
       <h1>Welcome everyone!</h1>
-      <h2>Login functionality coming soon!</h2>
     </div>
   );
 };
