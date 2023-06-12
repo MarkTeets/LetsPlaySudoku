58da00c24c86731224d27ad88f78553f50eb6063
Mark Teets <markteets@gmail.com>
2023-06-12 00:48:18 -0400

    Might switch error handling

--- server/controllers/puzzleController.js
+++ server/controllers/puzzleController.js
@@ -1,4 +1,5 @@
-const models = require('../models/puzzleModel');
+const models = {};
+models.Puzzle = require('../models/puzzleModel');
 // import { isValidPuzzle } from '../../client/data/squares';
 
 const puzzleController = {};
@@ -66,3 +67,58 @@ const errorMaker = (middleware, status, message, err) => {
 
   return error;
 };
+
+
+/*
+puzzleController.getPuzzle = async (req, res, next) => {
+  //given a string query, request the resource from the 
+  //database and return it as a json
+  try {
+    const { puzzleNumber } = req.query;
+    // console.log('puzzleNumber:', puzzleNumber);
+
+    if (puzzleNumber === undefined) {
+      return next(createErr({
+        method: 'getPuzzle',
+        type: 'destructing puzzleNumber from req.query',
+        status: 400,
+        err: 'Failed to retrieve puzzleNumber query string from req.query'
+      }));
+    }
+
+    res.locals.puzzleObj = await models.Puzzle.findOne({ number: puzzleNumber }).exec();
+
+    if (res.locals.puzzleObj === null) {
+      return next(createErr({
+        method: 'getPuzzle',
+        type: 'MongoDB findOne returned null',
+        status: 400,
+        err: 'Failed to find and return puzzle corresponding to #${puzzleNumber}'
+      }));
+    }
+
+    return next();
+
+  } catch (err) {
+    return next(createErr({
+      method: 'getPuzzle',
+      type: 'Failed to retrieve puzzle string',
+      status: 500,
+      err
+    }));
+  }
+};
+
+// Error generation helper function
+const createErr = ({ method, type, status, err }) => {
+  const errorObj = {
+    log: `puzzleController.${method} ${type}: ERROR: ${typeof err === 'object' ? err.message : err}`,
+    message: { err: `Error occurred in puzzleController.${method}. Check server logs for more details.` }
+  };
+  if (status) {
+    errorObj.status = status;
+  }
+  return errorObj;
+};
+
+*/
