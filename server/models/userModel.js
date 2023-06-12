b11932dde7a5760dd8d32871d077c29b077ae416
Mark Teets <markteets@gmail.com>
2023-06-12 00:51:33 -0400

    changed nickname to displayName

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  nickname: String,
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  displayName: String,
   allPuzzles: [
     {
       progress: String,
