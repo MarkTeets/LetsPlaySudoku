32d0e36d78b5e376e4a71b6ea1924128988c2914
Mark Teets <markteets@gmail.com>
2023-06-29 21:45:24 -0600

    Disabled future feature buttons

--- client/pages/puzzle/NewPuzzleSelect.jsx
+++ client/pages/puzzle/NewPuzzleSelect.jsx
@@ -32,33 +32,51 @@ const NewPuzzleSelect = () => {
   return (
     <>
       <h1>New puzzle select</h1>
+      <h3>Disabled buttons represent features that are on their way</h3>
       <div className='difficulty-selection-container'>
-        <button>Easy</button>
-        <button>Medium</button>
-        <button>Hard</button>
+        <button disabled>Easy</button>
+        <button disabled>Medium</button>
+        <button disabled>Hard</button>
         <button>Random</button>
       </div>
       <h2>Puzzle Filters:</h2>
       <div className="puzzle-filters">
         <div>difficultyScore</div>
-        <div> <button>X</button> uniqueSolution </div>
-        <div> <button>X</button> singleCandidate </div>
-        <div> <button>X</button> singlePosition </div>
-        <div> <button>X</button> candidateLines </div>
-        <div> <button>X</button> doublePairs </div>
-        <div> <button>X</button> multipleLines </div>
-        <div> <button>X</button> nakedPair </div>
-        <div> <button>X</button> hiddenPair </div>
-        <div> <button>X</button> nakedTriple </div>
-        <div> <button>X</button> hiddenTriple </div>
-        <div> <button>X</button> xWing </div>
-        <div> <button>X</button> forcingChains </div>
-        <div> <button>X</button> nakedQuad </div>
-        <div> <button>X</button> hiddenQuad </div>
-        <div> <button>X</button> swordfish </div>
+        <div> <button disabled>X</button> uniqueSolution </div>
+        <div> <button disabled>X</button> singleCandidate </div>
+        <div> <button disabled>X</button> singlePosition </div>
+        <div> <button disabled>X</button> candidateLines </div>
+        <div> <button disabled>X</button> doublePairs </div>
+        <div> <button disabled>X</button> multipleLines </div>
+        <div> <button disabled>X</button> nakedPair </div>
+        <div> <button disabled>X</button> hiddenPair </div>
+        <div> <button disabled>X</button> nakedTriple </div>
+        <div> <button disabled>X</button> hiddenTriple </div>
+        <div> <button disabled>X</button> xWing </div>
+        <div> <button disabled>X</button> forcingChains </div>
+        <div> <button disabled>X</button> nakedQuad </div>
+        <div> <button disabled>X</button> hiddenQuad </div>
+        <div> <button disabled>X</button> swordfish </div>
       </div>
     </>
   );
 };
 
 export default NewPuzzleSelect;
+
+// Wrote this before realizing I'd like to use this logic in the backend
+// Saving this code until I use it there
+
+// const puzzleSelectWithoutFilters = (user, puzzleRangeStart, puzzleRangeEnd) => {
+//   const takenNumbers = new Set();
+//   for (const puzzleObj of user.allPuzzles) {
+//     takenNumbers.add(puzzleObj.puzzleNumber);
+//   }
+//   let numberIsValid = false;
+//   let puzzleNumber;
+//   while (!numberIsValid) {
+//     puzzleNumber = Math.floor(Math.random() * (puzzleRangeEnd - puzzleRangeStart) + puzzleRangeStart);
+//     if (!takenNumbers.has(puzzleNumber)) numberIsValid = true;
+//   }
+//   return puzzleNumber;
+// };
