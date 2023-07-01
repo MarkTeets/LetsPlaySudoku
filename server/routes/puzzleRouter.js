b729af384bd143b177ffee10fadc29cd595f60ed
Mark Teets <markteets@gmail.com>
2023-07-01 02:17:25 -0600

    Started routes for numerical new puzzle retrieval

--- server/routes/puzzleRouter.js
+++ server/routes/puzzleRouter.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const puzzleRouter = express.Router();
+const userController = require('../controllers/userController');
 const puzzleController = require('../controllers/puzzleController');
 
 puzzleRouter.get('/',
+  puzzleController.getPuzzleByNumber,
+  (req, res) => {
+    res.status(200).json(res.locals.puzzleObj);
+  }
+);
+
+puzzleRouter.post('/get-next-puzzle-for-user',
+  userController.getUser,
+  puzzleController.getNextPuzzleForUser,
+  puzzleController.getPuzzleByNumber,
+  (req, res) => {
+    res.status(200).json(res.locals.puzzleObj);
+  }
+);
+
+puzzleRouter.post('/get-next-puzzle-for-guest',
+  puzzleController.getNextPuzzleForGuest,
   puzzleController.getPuzzleByNumber,
   (req, res) => {
     res.status(200).json(res.locals.puzzleObj);
