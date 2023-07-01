598e29e4047a987e04cd9178fd579441c3b99a9a
Mark Teets <markteets@gmail.com>
2023-07-01 02:01:29 -0600

    Changed layouts and puzzle loading process

--- client/App.jsx
+++ client/App.jsx
@@ -1,24 +1,26 @@
 import React, { useState, useRef } from 'react';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 
-// pages, loaders
-import Home from './pages/Welcome/Home';
-import { PuzzlePage, puzzleLoader, puzzleTestLoader } from './pages/Puzzle/PuzzlePage';
-import Login, { loginAction } from './pages/Welcome/Login';
-import SignUp, { signUpAction } from './pages/Welcome/SignUp';
-import UserHome from './pages/User/UserHome';
-import NewPuzzleSelect from './pages/Puzzle/NewPuzzleSelect';
-import NotFound from './pages/NotFound';
-import ErrorPage from './pages/ErrorPage';
-
 // layouts
 import RootLayout from './layouts/RootLayout';
 import WelcomeLayout from './layouts/WelcomeLayout';
-import GuestLayout from './layouts/GuestLayout';
 import UserLayout from './layouts/UserLayout';
 
+// pages, loaders
+import Home from './pages/Welcome/Home';
+import { PuzzlePage } from './pages/Puzzle/PuzzlePage';
+import { PuzzlePageTest, puzzleTestLoader } from './pages/Puzzle/PuzzlePageTest';
+import Login, { loginAction } from './pages/Welcome/Login';
+import SignUp, { signUpAction } from './pages/Welcome/SignUp';
+import UserHome from './pages/User/UserHome';
+import PuzzleSelectViaFilters from './pages/PuzzleSelect/PuzzleSelectViaFilters';
+import PuzzleSelectMenu from './pages/PuzzleSelect/PuzzleSelectMenu';
+import SavedPuzzleSelect from './pages/PuzzleSelect/SavedPuzzleSelect';
+import NotFound from './pages/NotFound';
+import ErrorPage from './pages/ErrorPage';
+
 // context
-import { userContext, pageContext } from './context';
+import { userContext, puzzleCollectionContext, pageContext } from './context';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,6 +30,7 @@ const router = createBrowserRouter(
       errorElement={<ErrorPage />}
     >
 
+      {/* Welcome layout */}
       <Route path='/' element={<WelcomeLayout />}>
         <Route index element={<Home />} />
         <Route
@@ -44,41 +47,36 @@ const router = createBrowserRouter(
         />
       </Route>
 
-      <Route path='guest' element={<GuestLayout/>}>
-        <Route
-          index
-          element={<NewPuzzleSelect key='NewPuzzleSelect' />}
-        />
-        <Route
-          path='playTest'
-          element={<PuzzlePage key='PuzzlePageContainerFromPlayTest' />}
-          loader={puzzleTestLoader}
-        />
-        <Route
-          path='play/:puzzleNumber'
-          element={<PuzzlePage key='PuzzlePageContainerFromDB' />}
-          loader={puzzleLoader}
-        />
-      </Route>
-
+      {/* User layout */}
       <Route path=':username' element={<UserLayout/>}>
-        <Route
+        {/* <Route
           index
           element={<UserHome key='UserHome' />}
+        /> */}
+        <Route
+          index
+          element={<PuzzleSelectMenu key='PuzzleSelectMenu' />}
+        />
+        {/* <Route
+          path='PuzzleSelectMenu'
+          element={<PuzzleSelectMenu key='PuzzleSelectMenu' />}
+        /> */}
+        <Route
+          path='savedPuzzleSelect'
+          element={<SavedPuzzleSelect key='SavedPuzzleSelect' />}
         />
         <Route
-          path='newPuzzleSelect'
-          element={<NewPuzzleSelect key='NewPuzzleSelect' />}
+          path='puzzleSelectViaFilters'
+          element={<PuzzleSelectViaFilters key='PuzzleSelectViaFilters' />}
         />
         <Route
           path='playTest'
-          element={<PuzzlePage key='PuzzlePageContainerFromPlayTest' />}
+          element={<PuzzlePageTest key='PuzzlePageTest' />}
           loader={puzzleTestLoader}
         />
         <Route
           path='play/:puzzleNumber'
           element={<PuzzlePage key='PuzzlePageContainerFromDB' />}
-          loader={puzzleLoader}
         />
       </Route>
       
@@ -90,15 +88,18 @@ const router = createBrowserRouter(
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [puzzleCollection, setPuzzleCollection] = useState({ });
   const pageInfo = useRef(null);
 
   // console.log('user from app', user);
 
   return (
-    <userContext.Provider value={{ user, setUser }}> 
-      <pageContext.Provider value={{pageInfo}}>
-        <RouterProvider router={router} />
-      </pageContext.Provider>
+    <userContext.Provider value={{ user, setUser }}>
+      <puzzleCollectionContext.Provider value={{ puzzleCollection, setPuzzleCollection }}>
+        <pageContext.Provider value={{pageInfo}}>
+          <RouterProvider router={router} />
+        </pageContext.Provider>
+      </puzzleCollectionContext.Provider>
     </userContext.Provider>  
   );
 };
