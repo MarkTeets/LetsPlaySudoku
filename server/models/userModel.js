f7b8ecc4b35f076343447284ae5de44542a64f3d
Mark Teets <markteets@gmail.com>
2023-07-01 02:16:31 -0600

    Added last puzzleNumber so users can resume easily

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -6,14 +6,11 @@ const userSchema = new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   displayName: String,
+  lastPuzzleNumber: {type: Number, default: 0},
   allPuzzles: [
     {
       puzzleNumber: Number,
       progress: String,
-      // puzzleId: {
-      //   type: Schema.Types.ObjectId,
-      //   ref: 'Puzzle'
-      // }
     }
   ]
 });
