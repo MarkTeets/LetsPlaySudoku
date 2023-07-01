a9e3e2fa4384f84acc91f66ca4d268f2393f2c61
Mark Teets <markteets@gmail.com>
2023-07-01 02:15:40 -0600

    Updated for user puzzle retrieval

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
+const puzzleController = require('../controllers/puzzleController');
 
 
 userRouter.post('/signup',
   userController.getUser,
   userController.createUser,
   (req, res) => {
-    res.status(200).json(res.locals.frontendUser);
+    res.status(200).json(res.locals.frontendData);
   }
 );
 
 userRouter.post('/login',
   userController.getUser,
   userController.verifyUser,
+  puzzleController.getUserPuzzles,
   (req, res) => {
-    res.status(200).json(res.locals.frontendUser);
+    res.status(200).json(res.locals.frontendData);
   }
 );
 
@@ -23,7 +25,7 @@ userRouter.post('/save-puzzle',
   userController.getUser,
   userController.savePuzzle,
   (req, res) => {
-    res.status(200).json(res.locals.frontendUser);
+    res.status(200).json(res.locals.frontendData);
   }
 );
 
