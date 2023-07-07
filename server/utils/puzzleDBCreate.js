26230402091b800b0da4bbc66dab830e4d962555
Mark Teets <markteets@gmail.com>
2023-07-07 02:00:43 -0600

    Added csv file puzzle data pipeline

--- server/utils/puzzleDBCreate.js
+++ server/utils/puzzleDBCreate.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const Puzzle = require('../models/puzzleModel');
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+
+const filePath = path.resolve(__dirname, '../../data/sudoku.csv');
+const limit = 500;
+const results = [];
 
 const MONGO_URI='mongodb+srv://markteets:PV0m4ZjwEg3wZwIT@sudoku-db.ox6sdpn.mongodb.net/?retryWrites=true&w=majority';
 
@@ -9,7 +16,39 @@ mongoose.connect(MONGO_URI, {
   .then(() => console.log('Connected to Mongo DB!'))
   .catch(err => console.log('Database error: ', err.message));
 
+fs.createReadStream(filePath)
+  .pipe(csv())
+  .on('data', (data) => {
+    if (results.length <= limit) {
+      results.push(data);
+    }
+  })
+  .on('end', () => {
+    console.log(results);
+    populatePuzzles(results);
+  });
 
+
+const populatePuzzles = async (puzzleArray) => {
+  for (let i = 0; i < puzzleArray.length; i++){
+    const puzzleObj = {
+      puzzleNumber: i + 1,
+      puzzle: puzzleArray[i].puzzle,
+      solution: puzzleArray[i].solution
+    };
+    // console.log('i:', i);
+    // console.log('puzzleObj:', puzzleObj);
+    try {
+      const made = await Puzzle.create(puzzleObj);
+      // console.log('Saved puzzle', made.puzzleNumber);
+    } catch (err) {
+      console.log('Error in puzzle creation:', err.message);
+    }
+  }
+};
+  
+
+/* Old code used to manually enter puzzles
 const puzzle1 = '070000043040009610800634900094052000358460020000800530080070091902100005007040802';
 const solution1 = '679518243543729618821634957794352186358461729216897534485276391962183475137945862';
 
@@ -39,3 +78,4 @@ const initialPuzzle = async () => {
 };
 
 initialPuzzle();
+*/
