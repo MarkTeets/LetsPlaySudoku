78cabe0fc10d0960a46eef40accff80739e69544
Mark Teets <markteets@gmail.com>
2023-09-07 12:36:59 -0600

    Removed unecessary element

--- src/client/layouts/RootLayout.tsx
+++ src/client/layouts/RootLayout.tsx
@@ -3,10 +3,8 @@ import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
   return (
-    <div className='root-layout'>
-      <main>
-        <Outlet />
-      </main>
+    <div id='root-layout'>
+      <Outlet />
     </div>
   );
 };
