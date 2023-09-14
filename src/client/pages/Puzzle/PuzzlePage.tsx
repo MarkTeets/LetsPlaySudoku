54f591eafa474b06b99a818b4b76f7876c58a4a1
Mark Teets <markteets@gmail.com>
2023-09-14 06:11:44 -0600

    Updated to use pure functions for state

--- src/client/pages/Puzzle/PuzzlePage.tsx
+++ src/client/pages/Puzzle/PuzzlePage.tsx
@@ -3,14 +3,15 @@ import { useParams } from 'react-router-dom';
 
 // Types
 import {
+  InitialSquares,
   FilledSquares,
   PencilSquares,
-  NumberSelectBarProps,
-  SquareContextValue,
   ClickedSquare,
+  SquareContextValue,
   UserContextValue,
   PuzzleCollectionContextValue,
-  PageContextValue
+  PageContextValue,
+  NumberSelectBarProps
 } from '../../frontendTypes';
 
 // Components
@@ -22,9 +23,7 @@ import { userContext, puzzleCollectionContext, squareContext, pageContext } from
 
 // Utilities
 import {
-  getFilledSquares,
-  getPencilSquares,
-  getInitialFilledSquares,
+  initializeSquares,
   resetStateOnRefresh
 } from '../../utils/puzzle-functions/initialSquareStatePopulation';
 import { pencilSquaresFromString } from '../../utils/puzzle-functions/squaresFromPuzzleStrings';
@@ -44,21 +43,23 @@ const PuzzlePage = () => {
   const [clickedSquare, setClickedSquare] = useState<ClickedSquare>(null);
   const [showTools, setShowTools] = useState<boolean>(false);
 
-  // A separate cache is used to simultaneously calculate filledSquares and pencilSquares
-  // and account for their duplicates
-  const [filledSquares, setFilledSquares] = useState<FilledSquares>(() =>
-    getFilledSquares(puzzleNumber, user, puzzleCollection)
+  // initialSquares acts as a cache so that all square objects can be simultaneously calculated
+  // with their duplicates accounted for before the first render without repeating calculations
+  const [initialSquares, setInitialSquares] = useState<InitialSquares>(() =>
+    initializeSquares(puzzleNumber, user, puzzleCollection)
   );
-  const [pencilSquares, setPencilSquares] = useState<PencilSquares>(() => getPencilSquares());
+  const [filledSquares, setFilledSquares] = useState<FilledSquares>(initialSquares.filledSquares);
+  const [pencilSquares, setPencilSquares] = useState<PencilSquares>(initialSquares.pencilSquares);
 
   useEffect(() => {
     pageInfo.current = 'PuzzlePage';
   }, []);
 
   useEffect(() => {
-    // Needs to run every time so lastPuzzle is updated on refresh. At the moment, user isn't actually saved
-    // to database until user hits save so they're old last puzzle could be used in navbar without this useEffect.
-    // If there is a user, this puzzle number will be saved as their most recent puzzle for navigation purposes
+    // Needs to run every time so lastPuzzle is updated on refresh. At the moment, user isn't saved
+    // to database until user hits save so they're old last puzzle could be used in navbar without
+    // this useEffect. If there is a user, this puzzle number will be set as their most recent
+    // puzzle for navigation purposes
     if (user && user.lastPuzzle !== puzzleNumber) {
       setUser({
         ...user,
@@ -69,7 +70,14 @@ const PuzzlePage = () => {
 
   useEffect(() => {
     if (user && filledSquares.size === 0) {
-      resetStateOnRefresh(puzzleNumber, user, puzzleCollection, setFilledSquares, setPencilSquares);
+      resetStateOnRefresh(
+        puzzleNumber,
+        user,
+        puzzleCollection,
+        setInitialSquares,
+        setFilledSquares,
+        setPencilSquares
+      );
     }
   }, [user]);
 
@@ -91,18 +99,21 @@ const PuzzlePage = () => {
   //   renderCount.current += 1;
   // });
 
+  /** removeClickedSquareOnPuzzlePageBlur
+   *
+   * If a blur event is triggered for the puzzle-page-container, this function ensures that
+   * clickedSquare is set to null only if the user clicked outside of puzzle-page-container.
+   *
+   * @param e - React.FocusEvent
+   */
   const removeClickedSquareOnPuzzlePageBlur = (e: React.FocusEvent): void => {
-    // The following logic will make sure clickedSquare is set to null only if the user clicks outside of the puzzle-page-container
-    // Clicking outside of the puzzle-page-container, clicking on the puzzle-page-container and then a button within it,
-    // clicking from a button back to the puzzle-page-container, or clicking from button to buttons within puzzle-page-container
-    // all trigger the onBlur event as focus is changing.
-
-    // e.currentTarget is the element with the listener attached to it, in this case puzzle-page-container
-    // For a focus/blur event, e.relatedTarget will be the element that triggered the blur event,
-    // aka the element clicked on.
-    // e.currentTarget.contains(e.relatedTarget) will be true changing focus within the puzzle-page-container
-    // e.currentTarget === e.relatedTarget will be true if transitioning from focusing on a button to the puzzle-page-container
-
+    /**
+     * e.currentTarget is the element with the listener attached to it, aka puzzle-page-container.
+     * e.relatedTarget is the element that triggers the blur event, aka the element clicked on.
+     * e.currentTarget.contains(e.relatedTarget) will be true changing focus within the
+     * puzzle-page-container; e.currentTarget === e.relatedTarget will be true if transitioning
+     * from focusing on a button to the puzzle-page-container
+     */
     if (!(e.currentTarget === e.relatedTarget || e.currentTarget.contains(e.relatedTarget))) {
       setClickedSquare(null);
     }
@@ -125,7 +136,7 @@ const PuzzlePage = () => {
   };
 
   const resetPuzzle = (): void => {
-    setFilledSquares(getInitialFilledSquares());
+    setFilledSquares(initialSquares.originalPuzzleFilledSquares);
     setPencilSquares(pencilSquaresFromString());
     setClickedSquare(null);
     setPencilMode(false);
@@ -164,7 +175,6 @@ const PuzzlePage = () => {
         <h1>Loading</h1>
       ) : (
         <squareContext.Provider value={SquareContextValue}>
-          {/* puzzle-page div is here to reduce the size of the puzzle-page-container for the onBlur event*/}
           <div id='puzzle-page-centerer'>
             <div
               id='puzzle-page-container'
--- src/client/utils/puzzle-functions/initialSquareStatePopulation.ts
+++ src/client/utils/puzzle-functions/initialSquareStatePopulation.ts
@@ -1,6 +1,5 @@
 // Types
-import { ResetStateOnRefresh, SquaresStateInitializerCache } from '../../frontendTypes';
-import { User, PuzzleCollection } from '../../../types';
+import { InitialSquares, InitializeSquares, ResetStateOnRefresh } from '../../frontendTypes';
 
 // Utilities
 import {
@@ -13,65 +12,68 @@ import {
   updatePencilSquaresDuplicates
 } from './updateSquaresDuplicates';
 
-const cache: SquaresStateInitializerCache = {
-  puzzleNumber: 0,
-  initialFilledSquares: filledSquaresFromString(),
-  filledSquares: filledSquaresFromString(),
-  pencilSquares: pencilSquaresFromString()
-};
+/** initializeSquares
+ *
+ * Generates an InitialSquares object based on the puzzleNumber and the puzzle associated with
+ * said number on the user object and the puzzleCollection object. This initialSquares object holds
+ * a filledSquares object built only from the original puzzle, as well as a filledSquares object
+ * updated by a users progress string and a pencilSquares object, both having updated duplicate
+ * properties.
+ *
+ * @param puzzleNumber
+ * @param user
+ * @param puzzleCollection
+ * @returns InitialSquares object
+ */
+export const initializeSquares: InitializeSquares = (puzzleNumber, user, puzzleCollection) => {
+  const initialSquares: InitialSquares = {
+    originalPuzzleFilledSquares: filledSquaresFromString(),
+    filledSquares: filledSquaresFromString(),
+    pencilSquares: pencilSquaresFromString()
+  };
 
-const fillSquaresStateCache = (
-  puzzleNumber: number,
-  user: User,
-  puzzleCollection: PuzzleCollection
-) => {
-  // console.log('Generating new cache');
-  cache.puzzleNumber = puzzleNumber;
-  cache.initialFilledSquares = filledSquaresFromString(puzzleCollection[puzzleNumber]?.puzzle);
-  cache.filledSquares = updateFilledSquaresFromProgress(
-    cache.initialFilledSquares,
+  if (!user) return initialSquares;
+
+  initialSquares.originalPuzzleFilledSquares = filledSquaresFromString(
+    puzzleCollection[puzzleNumber]?.puzzle
+  );
+  initialSquares.filledSquares = updateFilledSquaresFromProgress(
+    initialSquares.originalPuzzleFilledSquares,
     puzzleNumber,
     user,
     puzzleCollection
   );
-  cache.pencilSquares = pencilSquaresFromString(user?.allPuzzles[puzzleNumber]?.pencilProgress);
-  updateFilledSquaresDuplicates(cache.filledSquares, cache.pencilSquares);
-  updatePencilSquaresDuplicates(cache.filledSquares, cache.pencilSquares);
+  initialSquares.pencilSquares = pencilSquaresFromString(
+    user?.allPuzzles[puzzleNumber]?.pencilProgress
+  );
+  updateFilledSquaresDuplicates(initialSquares.filledSquares, initialSquares.pencilSquares);
+  updatePencilSquaresDuplicates(initialSquares.filledSquares, initialSquares.pencilSquares);
+  return initialSquares;
 };
 
-const cacheChecker = (puzzleNumber: number, user: User, puzzleCollection: PuzzleCollection) => {
-  if (user && (cache.puzzleNumber === 0 || cache.puzzleNumber !== puzzleNumber)) {
-    fillSquaresStateCache(puzzleNumber, user, puzzleCollection);
-  }
-};
-
-export const getFilledSquares = (
-  puzzleNumber: number,
-  user: User,
-  puzzleCollection: PuzzleCollection
-) => {
-  // console.log('get squares called');
-  cacheChecker(puzzleNumber, user, puzzleCollection);
-  // fillSquaresStateCache(puzzleNumber, user, puzzleCollection);
-  return cache.filledSquares;
-};
-
-export const getPencilSquares = () => {
-  return cache.pencilSquares;
-};
-
-export const getInitialFilledSquares = () => {
-  return cache.initialFilledSquares;
-};
-
+/** resetStateOnRefresh
+ *
+ * When the page is refreshed, the user stored in the state will be lost. Therefore the first render
+ * will produce an empty Sudoku grid. This method will calculate the Sudoku grid and set its state
+ * appropriately
+ *
+ * @param puzzleNumber
+ * @param user
+ * @param puzzleCollection
+ * @param setInitialSquares
+ * @param setFilledSquares
+ * @param setPencilSquares
+ */
 export const resetStateOnRefresh: ResetStateOnRefresh = (
   puzzleNumber,
   user,
   puzzleCollection,
+  setInitialSquares,
   setFilledSquares,
   setPencilSquares
 ) => {
-  fillSquaresStateCache(puzzleNumber, user, puzzleCollection);
-  setFilledSquares(cache.filledSquares);
-  setPencilSquares(cache.pencilSquares);
+  const resetSquares = initializeSquares(puzzleNumber, user, puzzleCollection);
+  setInitialSquares(resetSquares);
+  setFilledSquares(resetSquares.filledSquares);
+  setPencilSquares(resetSquares.pencilSquares);
 };
